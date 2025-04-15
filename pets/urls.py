a785e421a59f3a4770e79782da2483a18be674c5
KaylerMm <kaylermoura.km@example.com>
2025-04-15 19:22:40 -0300

    feat: Pet details screen

--- pets/urls.py
+++ pets/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.pet_list, name='pet_list'),
+    path('<int:pk>/', views.pet_detail, name='pet_detail'),
     path('create/', views.create_pet, name='create_pet'),
     path('edit/<int:id>/', views.edit_pet, name='edit_pet'),
     path('delete/<int:id>/', views.delete_pet, name='delete_pet'),
