db274d7c2984e4442c41617b588e72e7a2757eea
Kayler Moura <kaylermoura.km@gmail.com>
2025-04-16 19:57:24 -0300

    CRUD for Medical Record

--- appointments/urls.py
+++ appointments/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('create/', views.appointment_create, name='appointment_create'),
     path('<int:pk>/edit/', views.appointment_update, name='appointment_update'),
     path('<int:pk>/delete/', views.appointment_delete, name='appointment_delete'),
+    path('<int:appointment_id>/medical-record/', views.view_medical_record, name='view_medical_record'),
+    path('<int:appointment_id>/medical-record/create/', views.create_medical_record, name='create_medical_record'),
+    path('<int:appointment_id>/medical_record_edit/', views.edit_medical_record, name='edit_medical_record'),
+    path('appointments/<int:appointment_id>/medical_record_delete/', views.delete_medical_record, name='delete_medical_record'),
 ]
